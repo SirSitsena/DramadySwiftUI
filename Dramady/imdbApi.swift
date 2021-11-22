f55f4bace07f18bfb5942bbfb99255f9b3ce4ab0
rasmus kolmodin <rasmus.kolmodin@gmail.com>
2021-11-22 17:46:48 +0300

    Added imdbApi file, not corrupted this time /Rasmus

---
+++ Dramady/imdbApi.swift
@@ -0,0 +1,15 @@
+//
+//  imdFile.swift
+//  Dramady
+//
+//  Created by User on 22.11.2021.
+//
+
+import Foundation
+
+class apiMediator {
+    //Fetching a single movie
+    func fetchMovie() {
+        //TODO
+    }
+}
