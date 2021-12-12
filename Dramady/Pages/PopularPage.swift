c34a36ce3c9c1c594373f4b795b718367c28919d
Anestis Cheimonettos <anestisukr@gmail.com>
2021-12-12 14:31:01 +0300

    Popular Page fix

--- Dramady/Pages/PopularPage.swift
+++ Dramady/Pages/PopularPage.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct PopularPage: View {
     @State private var movies = [popularMovie]()
+    @ObservedObject var imageFetcher = ImageFetcher()
+    @State var images: [String: UIImage] = [:]
     var body: some View {
         List(movies, id: \.id) {movie in
             VStack {
-                Text(movie.title)
-                Text(movie.fullTitle)
-                Text(movie.rank)
+                HStack{
+                    AsyncImage(url: URL(string: movie.image)) { phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .frame(width: 50, height: 50, alignment: .leading)
+                        } else if phase.error != nil {
+                            Color.red
+                        } else {
+                            ProgressView()
+                            //Color.blue
+                        }
+                    }
+                    
+                    Text(movie.title)
+                    Text(movie.rank)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
             }
         }.task {
             Api().fetchPopular { (popularMovies) in
@@ -23,6 +40,7 @@ struct PopularPage: View {
         }
     }
 }
+
 
 struct PopularPage_Previews: PreviewProvider {
     static var previews: some View {
